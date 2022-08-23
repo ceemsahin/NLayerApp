c990f590d7f9de193abb38b6a7a5ee3b11712f98
CEM ŞAHİN <cmshn02@gmail.com>
2022-08-23 14:33:10 +0300

    Add project files.

---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+
+            builder.HasData(new Product
+            {
+                Id=1,
+                CategoryId=1,
+                Name="Kalem 1",
+                Price=100,
+                Stock=20,
+                CreatedDate=DateTime.Now
+
+            },
+            new Product
+            {
+                Id = 2,
+                CategoryId = 1,
+                Price = 120,
+                Name="Kalem 2",
+                Stock = 30,
+                CreatedDate = DateTime.Now
+
+            }
+            ,
+            new Product
+            {
+                Id = 3,
+                CategoryId = 1,
+                Price = 150,
+                Name = "Kalem 3",
+                Stock = 40,
+                CreatedDate = DateTime.Now
+
+            },
+            new Product
+            {
+                Id = 4,
+                CategoryId = 2,
+                Price = 100,
+                Name = "Kitap 1 ",
+                Stock = 70,
+                CreatedDate = DateTime.Now
+
+            },
+            new Product
+            {
+                Id = 5,
+                CategoryId = 2,
+                Price = 80,
+                Name = "Kitap 2 ",
+                Stock = 60,
+                CreatedDate = DateTime.Now
+
+            });
+
+
+
+
+
+        }
+    }
+}
---
+++ NLayer.Service/Services/ProductServiceNoWithCaching.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class ProductServiceNoWithCaching : Service<Product>, IProductService
+    {
+
+        private readonly IProductRepository _productrepository;
+        private readonly IMapper _mapper;
+
+        public ProductServiceNoWithCaching(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productrepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _productrepository = productrepository;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        {
+            var products = await  _productrepository.GetProductsWithCategory();
+            var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200,productsDto);
+        }
+    }
+}
